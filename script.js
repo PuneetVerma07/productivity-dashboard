dc7b5fd4f37600548c0e50657f5c58cfab7e8dcb
PuneetVerma07 <pk2382849@gmail.com>
2026-03-27 12:23:56 +0530

    feat: back button functionality added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let allElems = document.querySelectorAll(".elem")
 let fullElems = document.querySelectorAll(".fullElems")
+let fullElemsBackButton = document.querySelectorAll(".fullElems .back")
 
 allElems.forEach((elem) => {
     // console.log(elem.id)
@@ -7,5 +8,11 @@ allElems.forEach((elem) => {
     elem.addEventListener("click", function () {
         // console.log(fullElems[elem.id])
         fullElems[elem.id].style.display = "block"
+    })
+})
+
+fullElemsBackButton.forEach(function (back) {
+    back.addEventListener("click", function () {
+        fullElems[back.id].style.display = "none"
     })
 })
