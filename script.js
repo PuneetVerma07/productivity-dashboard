b2b0452571974614dfdf1e4c81f787c36fd776d2
PuneetVerma07 <pk2382849@gmail.com>
2026-03-28 12:03:49 +0530

    done: To-Do List UI done

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-let allElems = document.querySelectorAll(".elem")
-let fullElems = document.querySelectorAll(".fullElems")
-let fullElemsBackButton = document.querySelectorAll(".fullElems .back")
+function openFeatures() {
+    let allElems = document.querySelectorAll(".elem")
+    let fullElemsPage = document.querySelectorAll(".fullElems")
+    let fullElemsPageBackButton = document.querySelectorAll(".fullElems .back")
 
-allElems.forEach((elem) => {
-    // console.log(elem.id)
+    allElems.forEach((elem) => {
+        // console.log(elem.id)
 
-    elem.addEventListener("click", function () {
-        // console.log(fullElems[elem.id])
-        fullElems[elem.id].style.display = "block"
+        elem.addEventListener("click", function () {
+            // console.log(fullElems[elem.id])
+            fullElemsPage[elem.id].style.display = "block"
+        })
     })
-})
 
-fullElemsBackButton.forEach(function (back) {
-    back.addEventListener("click", function () {
-        fullElems[back.id].style.display = "none"
+    fullElemsPageBackButton.forEach(function (back) {
+        back.addEventListener("click", function () {
+            fullElemsPage[back.id].style.display = "none"
+        })
     })
+}
+
+// openFeatures()
+
+let form = document.querySelector(".addTask form")
+let taskInput = document.querySelector(".addTask form input")
+let taskDetailsInput = document.querySelector(".addTask form textarea")
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    
+    console.log(taskInput.value, taskDetailsInput.value)
 })
